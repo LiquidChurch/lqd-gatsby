2571e06ed8867478bf5cfb0685a3e712c3247644
george <george@liquidchurch.com>
2020-06-25 00:42:57 -0400

    Centralize svg icons

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -3,6 +3,8 @@ import { withBaseIcon } from 'react-icons-kit'
 import { ic_copyright } from 'react-icons-kit/md/ic_copyright'
 import { ic_arrow_forward } from 'react-icons-kit/md/ic_arrow_forward'
 
+import "./icons.css"
+
 const SmallIcon = withBaseIcon({size: 16, style: { color: '#B4B4B4', verticalAlign: 'text-bottom'}})
 const StandardIcon = withBaseIcon({size: 20, style: { color: '#B4B4B4', verticalAlign: 'text-bottom'}})
 
