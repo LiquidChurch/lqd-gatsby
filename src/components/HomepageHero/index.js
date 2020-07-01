352c7ea193879eb829c3bd538c77bd0f6588f974
george <george@liquidchurch.com>
2020-07-01 14:40:56 -0400

    Add in styles for homepage-header

--- src/components/HomepageHero/index.js
+++ src/components/HomepageHero/index.js
@@ -1,24 +1,43 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import Imgix from "react-imgix";
 
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button'
 
 import "./styles.css"
+
 /**
- * Page Hero component.
+ * Homepage Hero component.
  */
-export default ({ 
+export default ({
+  hero_tag,
   hero_text,
   hero_image,
-}) => {  
+  cta_label,
+  cta_url,
+}) => {
+  
+  useEffect(() => {
+    const timer1 = setTimeout(() => {
+      setRunEffect(!runEffect)
+      
+    }, 1500);
+    return () => clearTimeout(timer1);
+  }, []);
+  
+  const [runEffect, setRunEffect] = useState(false)
   return (
     <Container fluid className="homepage-hero-container">
       <Row>
-        <Col xs={12}>
-          <Image src={"https://liquidchurch.imgix.net" + hero_image} fluid/>
+        <Col xs={12} className={runEffect ? "homepage-hero-image loaded" : "homepage-hero-image"}>
+          <Imgix src={"https://liquidchurch.imgix.net" + hero_image + "?gam=50"} sizes="100vw" />
         </Col>
+        <span className={runEffect ? "homepage-hero-tag loaded" : "homepage-hero-tag"}>{hero_tag}</span>
+        <span className={runEffect ? "homepage-hero-text loaded" : "homepage-hero-text"}>{hero_text}</span>
+        <span className="homepage-hero-cta"><Button variant="primary" className="homepage-hero-button">{cta_label}</Button></span>
       </Row>
     </Container>
   )
