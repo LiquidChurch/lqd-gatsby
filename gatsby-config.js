48b28a730495d1ec1aeebbb6d5b7bb8bddeb4ba7
Dave Mackey <dave@davemackey.net>
2020-07-31 18:44:42 +0000

    Add support for dotenv, plugin to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -60,5 +62,13 @@ module.exports = {
         icon: `src/assets/images/logo-liquid-transparent.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require("./src/utils/algolia-queries")
+      }
+    }
   ],
 }
