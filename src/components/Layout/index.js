54fda2aaa2fd1fb9ef6c443267510f05fd0a1801
Dave Mackey <dave@davemackey.net>
2020-08-06 17:05:31 +0000

    Add search to layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Container from 'react-bootstrap/Container'
+import { Link } from "gatsby"
+import Search from "./search"
 
 import Meta from "../Meta"
 import Footer from "../Footer"
 import Header from "../Header"
 import "./styles.css"
+
+const searchIndices = [{ name: `Messages`, title: `Messages` }]
 
 export default ({ location, children }) => {
   
@@ -18,6 +22,7 @@ export default ({ location, children }) => {
     <Container fluid>
       <Meta />
       <Header home={"/" === location.pathname} location={location} />
+      <Search indicates={searchIndices} />
       <main id='main' className={`layout__content`} style={{minHeight: mainMinHeight + 'px'}}>{children}</main>
       <Footer />
     </Container>
