6533aec9d69058c009b9dcad006be08a0f5cb460
george <george@liquidchurch.com>
2020-12-04 10:18:11 -0500

    modifed footer styling

--- src/components/Footer/FooterBottom/index.js
+++ src/components/Footer/FooterBottom/index.js
@@ -9,12 +9,14 @@ import FooterCopyright from './footerCopyright.js'
 import "./styles.css"
 
 export default () => {
+  
+  //lg with t&c and pp is 4,5,3
   return (
   <>
-    <Col xs={{span: 12, order: 3}} sm={{span: 12, order: 3}} lg={{span: 4, order: 1}} block="true" className="footer-copyright-padding">
+    <Col xs={{span: 12, order: 3}} sm={{span: 12, order: 3}} lg={{span: 3, order: 1}} block="true" className="footer-copyright-padding">
       <FooterCopyright />
     </Col>
-    <Col xs={{span: 12, order:2}} sm={{span:6, order: 1}} lg={{span: 5, order: 2}} block="true">
+    <Col xs={{span: 12, order:2}} sm={{span:6, order: 1}} lg={{span: 6, order: 2}} block="true">
       <FooterMenu />
     </Col>
     <Col xs={{span: 12, order:1}} sm={{span:6, order: 2}} lg={{span: 3, order: 3}} block="true">
